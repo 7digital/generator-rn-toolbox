e446a05e5aaa15ad269ba6a6ce1e74313da014b4
Fabien Ryckoort <fabien.ryckoort@7digital.com>
2018-07-05 15:37:21 +0200

    fix(component): Get dominant image color, instead of color at 0,0

--- generators/assets/getPixelColor.js
+++ generators/assets/getPixelColor.js
@@ -1,14 +1,31 @@
 const gm = require('gm').subClass({ imageMagick: true });
 
-module.exports = (imagePath, x, y) =>
-  new Promise((resolve, reject) =>
+module.exports = (imagePath, x, y) => {
+  return new Promise((resolve, reject) => {
     gm(imagePath)
-      .crop(x, y)
-      .identify('%[hex:s]', (error, imageMagickColor) => {
+      .in('+dither')
+      .in('-colors', 5)
+      .in('-define', 'histogram:unique-colors=true')
+      .in('-format', '%c')
+      .write('histogram:info:-', (error, result) => {
         if (error) return reject(error);
-        if (imageMagickColor && imageMagickColor.length === 8) {
-          imageMagickColor = imageMagickColor.substr(0, 6);
+        const regex = /(?:\s+([0-9]+):.+(#[0-9A-F]{6}).*)/g;
+        let m;
+        let backgroundColor = '#000000';
+        let useTimes = 0;
+
+        while ((m = regex.exec(result)) !== null) {
+          // This is necessary to avoid infinite loops with zero-width matches
+          if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+          }
+          const count = parseInt(m[1], 10);
+          if (count > useTimes) {
+            useTimes = count;
+            backgroundColor = m[2];
+          }
         }
-        return resolve(`#${imageMagickColor}`);
-      })
-  );
+        return resolve(backgroundColor);
+      });
+  });
+};
